55a53716dd5581d9100fe02bc2fdaff964baf77a
Guru <139735814+Gurunath-S@users.noreply.github.com>
2026-02-27 13:20:47 +0530

    Update Config.js

--- client/Config.js
+++ client/Config.js
@@ -1,6 +1,2 @@
-// const BACKEND_URL = "https://time-management-coach-testing.onrender.com";
-
-// const BACKEND_URL = "https://testing-tmc-tst.onrender.com";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
-
 export default BACKEND_URL;
